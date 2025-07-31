9c1d3349fbff55f86e53152a0ed1ebe2bf017b53
Lucas Rodrigues Monteiro <lucas.monteiro@grupoorion.com.br>
2025-07-31 11:57:25 -0300

    secret word init

---
+++ src/data/word.js
@@ -0,0 +1,7 @@
+export const wordlist= {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olho"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+}
